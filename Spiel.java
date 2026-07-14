d23c13288b239ce51ccd286211d924f047d0a1af
JoMiNovi <98261597+JoMiNovi@users.noreply.github.com>
2026-07-14 11:04:56 +0200

    Vorlage hochgeladen

---
+++ Spiel.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+/**
+ * Klasse Spiel zur Umsetzung eines Mini-Projekts
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Spiel extends Ereignisbehandlung
+{
+    /**
+     * Notiere hier nötige Attribute.
+     */
+
+    /**
+     * Konstruktor für Objekte der Klasse Spiel:
+     * initialisiert die Attribute
+     */
+    public Spiel()
+    {
+        //hier ergänzen
+    }
+
+    /**
+     * Methode MausGeklickt
+     * 
+     * @param x x-Position des Klicks in Pixel
+     * @param y y-Position des Klicks in Pixel
+     * @param anzahl Klickanzahl, hier nicht relevant
+     */
+    @Override
+    void MausGeklickt(int x, int y, int anzahl)
+    {
+        //ggfs. ergänzen
+    }
+
+    /**
+     * Zeichnet abhängig von den Werten der Attribute neu
+     *
+     */
+    void AnzeigeAktualisieren()
+    {
+        //hier ergänzen
+    }
+
+}
